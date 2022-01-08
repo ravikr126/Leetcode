66cd70c3f81cd09f2c33b9f983e1f00dcddd70d0
Ravi Kumar <ravi14577@gmail.com>
2022-01-08 11:14:25 +0530

    Time: 81 ms (34.68%), Space: 9.2 MB (90.12%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,37 +1,52 @@
 class Solution {
 public:
     int cherryPickup(vector<vector<int>>& grid) {
-           //m=number of rows and n = number of columns
-        int m = (int)grid.size(), n = (grid[0].size());
-        //initializing m and n
-        vector<vector<vector<int>>> dp(m + 1, vector<vector<int>>(n, vector<int>(n, 0)));
-        //dp dimensions
-        dp[0][0][n - 1] = 0 == n - 1 ? grid[0][0] : grid[0][0] + grid[0][n - 1];
-        //row traversing
-        for (int row = 1; row < m; row++) {
-            //avoiding unnecessary column computations
-            for (int c1 = 0; c1 < min(n, row + 1); c1++) {
-                for (int c2 = max(0, n - 1 - row); c2 < n; c2++) {
-                    int prevMax = 0;
-                    //Moves of robos in columns
-                    for (int offset1 = max(0, c1 - 1); offset1 <= min(n - 1, c1 + 1); offset1++) {
-                        for (int offset2 = max(0, c2 - 1); offset2 <= min(n - 1, c2 + 1); offset2++) {
-                                prevMax = max(prevMax, dp[row - 1][offset1][offset2]); //9 states wediscussed can be summariazed 
-                        }
-                    }
-                    //Robo collision condition
-                    if (c1 == c2) dp[row][c1][c2] = prevMax + grid[row][c1];
-                    else dp[row][c1][c2] = prevMax + grid[row][c1] + grid[row][c2];
+        vector<int> dir={-1,0,1};
+        // let's define array size
+        int row = grid.size();
+        int col = grid[0].size();
+        int dp[row][col][col]; // we need to have a 3D array
+        
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                for(int k = 0; k < col; k++){
+                    dp[i][j][k] = -1;
                 }
             }
         }
-        int res = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                res = max(res, dp[m - 1][i][j]);    
+        int col1 = 0; // first robot position
+        int col2 = col - 1; // second robot position
+        
+        dp[0][col1][col2] = grid[0][col1] + grid[0][col2]; // we have the value of 0th row from where we will be starting;
+        
+        int ans = dp[0][col1][col2]; // intially we have the max value of the column that we will be started with;
+        
+        // now comes a part where we need to iterate over each row;
+        for(int i = 1; i < row; i++){
+            for(int c1 = 0; c1 < col; c1++){ // need to loop on each column precise in the row
+                for(int c2 = 0; c2 < col; c2++){
+                    // now from every column we need to fill the cube; for all the combinations or all the cell we can reach; with respective robot1 & robot2
+                    int prev = dp[i - 1][c1][c2];
+                    if(prev >= 0){
+                        for(int d1: dir){
+                            col1 = d1 + c1;
+                            for(int d2: dir){
+                                col2 = d2 + c2;
+                            
+                                if(inRange(col1, col) && inRange(col2, col)){
+                                    dp[i][col1][col2] = max(dp[i][col1][col2], prev+(col1 == col2 ? grid[i][col1] : (grid[i][col1] + grid[i][col2])));
+                                    ans = max(ans, dp[i][col1][col2]);
+                                }
+                            }
+                        }
+                    }
+                    
+                }
             }
         }
-        return res;
-    
+        return ans;
+    }
+    bool inRange(int val, int limit){
+        return 0 <= val && val < limit;
     }
 };
