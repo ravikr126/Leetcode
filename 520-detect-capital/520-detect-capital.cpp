4d80083d0caa7a3425751a836402faaad5312c1f
Ravi Kumar <ravi14577@gmail.com>
2022-01-24 09:07:23 +0530

    Time: 3 ms (39.46%), Space: 6.1 MB (67.65%) - LeetHub

--- 520-detect-capital/520-detect-capital.cpp
+++ 520-detect-capital/520-detect-capital.cpp
@@ -1,11 +1,32 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        for(int i=1;i<word.size();i++){
-            if( (isupper(word[1]) != isupper(word[i]))  ||  (islower(word[0]) && isupper(word[i])) )
-                return false;
+    //     for(int i=1;i<word.size();i++){
+    //         if( (isupper(word[1]) != isupper(word[i]))  ||  (islower(word[0]) && isupper(word[i])) )
+    //             return false;
+    //     }
+    //     return true;
+    // 
+    
+     int flag = 0, FLAG = 0;
+        
+        for(int i = 1; i < word.size(); i++)
+        {
+            if(word[i] >= 'A' && word[i] <= 'Z')
+                FLAG = 1;
+            if(word[i] >= 'a' && word[i] <= 'z')
+                flag = 1;
         }
-        return true;
+        
+        if(flag && FLAG)
+            return 0;
+        if(FLAG && word[0] >= 'a' && word[0] <= 'z')
+            return 0;
+        return 1;
+    
+    
+    
+    
     }
     
 };
