b1ea2f282efbf0cf62db38fd42c70b7d39717a19
Ravi Kumar <ravi14577@gmail.com>
2022-02-18 10:27:52 +0530

    Time: 4 ms (83.64%), Space: 7 MB (52.75%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.cpp
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -1,55 +1,23 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        // number of operation greater than length we return an empty string
-        if(num.length() <= k)   
-            return "0";
+    string ans="";
         
-        // k is 0 , no need of removing /  preforming any operation
-        if(k == 0)
-            return num;
-        
-        string res = "";// result string
-        stack <char> s; // char stack
-        
-        s.push(num[0]); // pushing first character into stack
-        
-        for(int i = 1; i<num.length(); ++i)
-        {
-            while(k > 0 && !s.empty() && num[i] < s.top())
-            {
-                // if k greater than 0 and our stack is not empty and the upcoming digit,
-                // is less than the current top than we will pop the stack top
-                --k;
-                s.pop();
+        for(auto it:num){
+            while(ans.size()!=0 and ans.back()>it and k!=0){
+                ans.pop_back();
+                k--;
             }
             
-            s.push(num[i]);
-            
-            // popping preceding zeroes
-            if(s.size() == 1 && num[i] == '0')
-                s.pop();
+            if(ans.size()!=0 or it!='0'){
+                ans.push_back(it);
+            }
         }
         
-        while(k && !s.empty())
-        {
-            // for cases like "456" where every num[i] > num.top()
-            --k;
-            s.pop();
+        while(ans.length()!=0 and k--){
+            ans.pop_back();
         }
         
-        while(!s.empty())
-        {
-            res.push_back(s.top()); // pushing stack top to string
-            s.pop(); // pop the top element
-        }
-        
-        reverse(res.begin(),res.end()); // reverse the string 
-        
-        if(res.length() == 0)
-            return "0";
-        
-        return res;
-        
+        return ans.empty()?"0":ans;
     }
 };
