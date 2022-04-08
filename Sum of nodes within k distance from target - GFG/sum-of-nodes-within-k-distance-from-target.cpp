9d0e4515dad0e3555e47e5948ff3349bfe8389c9
Ravi Kumar <ravi14577@gmail.com>
2022-04-09 02:12:17 +0530

    Added solution - LeetHub

---
+++ Sum of nodes within k distance from target - GFG/sum-of-nodes-within-k-distance-from-target.cpp
@@ -0,0 +1,162 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+
+ // } Driver Code Ends
+/*
+// node structure:
+
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+*/
+
+class Solution{
+public:
+ unordered_map<Node*,Node*> mp;
+Node *start;
+int sum;
+set<Node*> s;
+void findParent(Node *root,Node *par,int target)
+{
+   if(!root) return;
+   if(root->data==target) start=root;
+   mp[root]=par;
+   findParent(root->left,root,target);
+   findParent(root->right,root,target);
+}
+void solve(Node *root,int k)
+{
+   if(!root) return;
+   if(k<0) return;
+   if(s.count(root)) return;
+   sum+=root->data;
+   s.insert(root);
+   solve(root->left,k-1);
+   solve(root->right,k-1);
+   solve(mp[root],k-1);
+}
+   int sum_at_distK(Node* root, int target, int k)
+   {
+       // code here
+       start=NULL;
+       Node *par=NULL;
+       findParent(root,par,target);
+       sum=0;
+       solve(start,k);
+       return sum;
+   }
+};
+
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    getchar();
+    
+    while(t--)
+    {
+        string s;
+        getline(cin,s);
+        Node* root = buildTree(s);
+        
+        int target,k;
+        cin>> target >> k;
+        getchar();
+        
+        Solution ob;
+        cout<< ob.sum_at_distK(root,target,k) << endl;
+    }
+	return 0;
+}
+
+  // } Driver Code Ends
