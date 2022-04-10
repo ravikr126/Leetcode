9c147517fc02ecbe7d0d083b97b255e1f15332a4
Ravi Kumar <ravi14577@gmail.com>
2022-04-10 09:58:01 +0530

    Added solution - LeetHub

---
+++ Minimum increment/decrement to make array non-Increasing - GFG/minimum-incrementdecrement-to-make-array-non-increasing.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+ // } Driver Code Ends
+
+
+class Solution{
+public:
+    int minOperations(int *a,int n)
+    {
+       int count = 0;
+		priority_queue<int ,vector<int>, greater<int>>pq; 
+		for (int i = 0; i < n; i++){ 
+			if (!pq.empty() && pq.top() < a[i])
+				count += a[i] - pq.top(),pq.push(a[i]),
+				pq.pop();
+			pq.push(a[i]); 
+		} 
+		return count;  
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int  a[n+1];
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+            
+        Solution ob;
+        cout<<ob.minOperations(a,n)<<endl;
+    }
+    
+	return 0;
+}  // } Driver Code Ends
