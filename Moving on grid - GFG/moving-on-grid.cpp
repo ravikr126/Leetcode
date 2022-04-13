a06500e60f657a2ecaa79b0c5b02b8606bb91670
Ravi Kumar <ravi14577@gmail.com>
2022-04-14 02:09:38 +0530

    Added solution - LeetHub

---
+++ Moving on grid - GFG/moving-on-grid.cpp
@@ -0,0 +1,31 @@
+// { Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+class Solution {
+  public:
+    string movOnGrid(int r, int c) {
+        // code here
+         swap(r,c);
+    r = (r - 1) & 3;
+    c = (c - 1) % 7;
+    return (r == c ? "ARYA" : "JON");
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int r, c;
+        cin >> r >> c;
+        Solution ob;
+        cout<<ob.movOnGrid(r,c)<<endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
