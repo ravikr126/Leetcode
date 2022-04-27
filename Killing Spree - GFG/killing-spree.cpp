eb4d83b6297657c8c60e2192b095af6f74bf6f75
Ravi Kumar <ravi14577@gmail.com>
2022-04-27 17:50:42 +0530

    Added solution - LeetHub

--- Killing Spree - GFG/killing-spree.cpp
+++ Killing Spree - GFG/killing-spree.cpp
@@ -9,8 +9,8 @@ public:
     long long int killinSpree(long long int n)
     {
         // Code Here
-          // Code Here
-       int i=1;
+          
+        int i=1;
        int c=0;
        while(n>0)
        {
@@ -19,8 +19,8 @@ public:
            c++;
            i++;
        }
-       return c;           
-    }
+       return c;                
+   }
 };
 
 
