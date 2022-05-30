f12bc1aebeeed9e91be37dab822dda899e7cab6a
Ravi Kumar <ravi14577@gmail.com>
2022-05-30 11:07:17 +0530

    Time: 30 ms, Memory: 22.4 MB - LeetHub

---
+++ valid-mountain-array/valid-mountain-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+          int N = arr.size();
+        int i = 0;
+
+        // walk up
+        while (i+1 < N && arr[i] < arr[i+1])
+            i++;
+
+        // peak can't be first or last
+        if (i == 0 || i == N-1)
+            return false;
+
+        // walk down
+        while (i+1 < N && arr[i] > arr[i+1])
+            i++;
+
+        return i == N-1;
+    }
+};
