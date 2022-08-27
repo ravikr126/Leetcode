f4f2a6e14e4ba050b0f1c4bb0ff69b164b6dbae6
Ravi Kumar <ravi14577@gmail.com>
2022-08-27 09:53:29 +0530

    Time: 118 ms (100.00%), Space: 10.5 MB (97.15%) - LeetHub

--- 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
+++ 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
@@ -1,84 +1,80 @@
 class Solution {
 public:
-    
-    // function for finding maximum subarray having sum less than k
-    
-    int find_max(vector<int>& arr, int k)
-    {
-        int n = arr.size();
-        
-        int maxi = INT_MIN;
-        
-        // curr_sum will store cumulative sum
-        
-        int curr_sum = 0;
-        
-        // set will store the prefix sum of array
-        
-        set<int> s;
-        
-        // put 0 into set, if curr_sum == k, (curr_sum - k) will be zero 
-        
-        s.insert(0);
-        
-        for(int i = 0; i < n; i++)
-        {
-            // calculate cumulative sum
-            
-            curr_sum += arr[i];
-            
-            // find the prefix sum in set having sum == curr_sum - k
-            
-            auto it = s.lower_bound(curr_sum - k);
-            
-            // if prefix sum is present, update the maxi
-            
-            if(it != s.end())
-            {
-                maxi = max(maxi, curr_sum - *it);
-            }
-            
-            // insert prefix sum into set
-            
-            s.insert(curr_sum);
-        }
-        
-        return maxi;
-    }
-    
+	// TIME COMPLEXITY:- O(M^2 NLOG(N))
+	// SPACE COMPLEXITY:- O(N)
     int maxSumSubmatrix(vector<vector<int>>& matrix, int k) {
+    //     int n = matrix.size();
+    //     int m = matrix[0].size();
+    //     int ans = INT_MIN;
+    //     // prefix sum of every row
+    //     for(int i=0;i<n;i++){
+    //         for(int j=1;j<m;j++)
+    //             matrix[i][j]+=matrix[i][j-1];
+    //     }
+    //     // try every possible width of subarray
+    //     for(int start=0;start<m;start++){
+    //         for(int end=start;end<m;end++){
+    //             set<int> s = {0};
+    //             int pref_sum = 0;
+    //             // for current width of rectangle
+    //             for(int i=0;i<n;i++){
+    //                 int sum = matrix[i][end];
+    //                 if(start>0)
+    //                     sum-=matrix[i][start-1];
+    //                 pref_sum+=sum;
+    //                 auto itr = s.lower_bound(pref_sum-k);
+    //                 if(itr!=s.end())
+    //                     ans = max(ans,pref_sum-(*itr));
+    //                 s.insert(pref_sum);
+    //             }
+    //         }
+    //     }
+    //     return ans;
+    
+        int ans = INT_MIN;
         
-        int n = matrix.size();
+        int R = matrix.size();
+        int C = matrix[0].size();
+        int arr[R];
         
-        int m = matrix[0].size();
-        
-        int maxi = INT_MIN;
-        
-        // fix the position two rows and take cumulative sum of columns between two fixed rows
-        
-        for(int start_row = 0; start_row < n; start_row++)
-        {
-            vector<int> col_array(m, 0);
+        for(int l = 0; l < C; ++l) {
+            memset(arr, 0, sizeof(arr));
             
-            for(int end_row = start_row; end_row < n; end_row++)
+            for(int r = l; r < C; ++r) 
             {
-                // take cumulative sum of columns between two fixed rows
-                
-                for(int col = 0; col < m; col++)
+                for(int up = 0; up < R; ++up)
                 {
-                    col_array[col] += matrix[end_row][col];
+                    arr[up] += matrix[up][r]; 
                 }
-                
-                // find maximum subarray having sum less than equal to k
-                
-                int curr_max = find_max(col_array, k);
-                
-                // update the maximum sum
-                
-                maxi = max(maxi, curr_max);
+                int sum = 0, g = INT_MIN;
+                for(int up = 0; up < R; ++up) 
+                {
+                    sum = max(sum + arr[up], arr[up]);
+                    g = max(g, sum);
+                    if(g == k) 
+                        return k;
+                }
+                if(g < k) 
+                {
+                    ans = max(ans, g);
+                } 
+                else 
+                {
+                    for(int up1 = 0; up1 < R; ++up1) 
+                    {
+                        sum = 0;
+                        for(int up2 = up1; up2 < R; ++up2) 
+                        {
+                            sum += arr[up2];
+                            if(sum <= k) 
+                                ans = max(ans, sum);
+                        }
+                        
+                        if(ans == k) return k;
+                    }
+                }
             }
         }
-        
-        return maxi;
+        return ans;
     }
 };
