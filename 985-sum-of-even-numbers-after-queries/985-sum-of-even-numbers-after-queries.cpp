bab8445fe52e0e0ea71d5244b21f0eacf25a5901
Ravi Kumar <ravi14577@gmail.com>
2022-09-21 18:15:30 +0530

    Time: 245 ms (23.21%), Space: 49.7 MB (20.44%) - LeetHub

---
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    // the idea is we don't calculate the even sum from scratch for each query
+    // instead, we calculate it at the beginning
+    // since each query only updates one value, 
+    // so we can adjust the even sum base on the original value and new value
+    vector<int> sumEvenAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
+        int evenSum = 0;
+        // calculate the sum of all even numbers
+        for (auto x : nums) {
+            if (x % 2 == 0) {
+                evenSum += x;
+            }
+        }
+        vector<int> ans;
+        for (auto q : queries) {
+            int val = q[0], idx = q[1];
+            // if original nums[idx] is even, then we deduct it from evenSum
+            if (nums[idx] % 2 == 0) evenSum -= nums[idx];
+            // in-place update nums
+            nums[idx] += val;
+            // check if we need to update evenSum for the new value
+            if (nums[idx] % 2 == 0) evenSum += nums[idx];
+            // then we have evenSum after this query, push it to ans 
+            ans.push_back(evenSum);
+        }
+        return ans;
+    }
+};
