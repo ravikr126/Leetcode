31115062bfb9746fdd5d17ec298069ef0d71af84
Ravi Kumar <ravi14577@gmail.com>
2022-10-04 09:31:47 +0530

    Time: 30 ms (11.00%), Space: 21.3 MB (74.14%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -9,13 +9,37 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
 class Solution {
 public:
+    bool fun(TreeNode *node, int req){
+        if(!node) return false;
+        int x = req - node -> val;
+        if(node -> left == NULL && node -> right == NULL){
+            return (x == 0);
+        }
+        return fun(node -> left, x) || fun(node -> right, x);
+        
+    }
     bool hasPathSum(TreeNode* root, int targetSum) {
-       if(!root)
-            return false;
-        if(!root->left && !root->right && targetSum - root->val == 0)
-            return true;
-        return (hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val));
+        return fun(root, targetSum);
     }
 };
+
+
+
+
+
+//2nd sol
+
+// class Solution {
+// public:
+//     bool hasPathSum(TreeNode* root, int targetSum) {
+//        if(!root)
+//             return false;
+//         if(!root->left && !root->right && targetSum - root->val == 0)
+//             return true;
+//         return (hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val));
+//     }
+// };
