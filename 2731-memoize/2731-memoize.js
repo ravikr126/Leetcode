a1efb37d0f50b8621d2f934bc8f27dd1eaa61429
Ravi Kumar <ravi14577@gmail.com>
2025-05-28 23:28:25 +0530

    Time: 251 ms (76.22%), Space: 94.3 MB (72.97%) - LeetHub

---
+++ 2731-memoize/2731-memoize.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Function} fn
+ */
+function memoize(fn) {
+    
+   const cache = {};
+  
+   return function(...args) {
+    const key = JSON.stringify(args);
+    
+    if (key in cache) {
+      return cache[key];
+    }
+    
+    const result = fn.apply(this, args);
+    cache[key] = result;
+    
+    return result;
+  }
+  
+}
