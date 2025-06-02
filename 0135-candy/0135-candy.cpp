66073dbcc586ca0746d185e19586534dd2f8b078
Ravi Kumar <ravi14577@gmail.com>
2025-06-02 13:11:35 +0530

    Time: 0 ms (100%), Space: 22.9 MB (43.5%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,37 +1,18 @@
 class Solution {
 public:
-    int candy(vector<int>& ratings) {
-       int n = ratings.size();
-        int totalCandies = n;
-        int i = 1;
+    int candy(vector<int>& ratings, int cnt = 0) {
+        int n = ratings.size();
+        vector<int> candies(n, 1);
 
-        while (i < n) {
-            if (ratings[i] == ratings[i - 1]) {
-                i++;
-                continue;
-            }
-
-            int currentPeak = 0;
-            while (i < n && ratings[i] > ratings[i - 1]) {
-                currentPeak++;
-                totalCandies += currentPeak;
-                i++;
-            }
-
-            if (i == n) {
-                return totalCandies;
-            }
-
-            int currentValley = 0;
-            while (i < n && ratings[i] < ratings[i - 1]) {
-                currentValley++;
-                totalCandies += currentValley;
-                i++;
-            }
-
-            totalCandies -= min(currentPeak, currentValley);
+        for (int i = 1; i < n; i++) 
+            if (ratings[i] > ratings[i - 1])
+                candies[i] = candies[i - 1] + 1;
+        
+        for (int i = n - 1; i > 0; i--) {
+            if (ratings[i - 1] > ratings[i])
+                candies[i - 1] = max(candies[i] + 1, candies[i - 1]);
+            cnt += candies[i - 1];
         }
-
-        return totalCandies;        
+        return cnt + candies[n - 1];
     }
 };
