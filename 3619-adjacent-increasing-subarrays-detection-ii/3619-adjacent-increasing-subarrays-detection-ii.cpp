29b5d4a913a5c6bd7a0f785e06f41effee1ed212
Ravi Kumar <ravi14577@gmail.com>
2025-10-15 22:33:29 +0530

    Time: 219 ms (77.44%), Space: 173.5 MB (98.12%) - LeetHub

--- 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.cpp
+++ 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.cpp
@@ -1,39 +1,19 @@
 class Solution {
 public:
-    bool f(int mid,vector<int>& suff,vector<int>& nums){
-        int i=0;
-        while(i<=(nums.size() - (2*mid))){
-            if(suff[i]>=mid && suff[i+mid]>=mid){
-                return true;
-            }
-            i++;
-        }
-        return false;
-    }
-    int binarySearch(vector<int>& nums,vector<int>& suff){
-        int left=1,right=nums.size()/2;
-        int result=0;
-        while(left<=right){
-            int mid = (left) + (right-left)/2;
-            if(f(mid,suff,nums)){
-                result = mid;
-                left=mid+1;
-            }
-            else{
-                right = mid-1;
-            }
-        }
-        return result;
-    }
     int maxIncreasingSubarrays(vector<int>& nums) {
-        int size=nums.size();
-        vector<int> suff(size,1);
-        for(int i=size-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                suff[i]=suff[i+1]+1;
+        int n = nums.size();
+        int up = 1, preUp = 0, res = 0;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i - 1]) up++;
+            else {
+                preUp = up;
+                up = 1;
             }
+            int half = up >> 1;
+            int m = min(preUp, up);
+            int candidate = max(half, m);
+            if (candidate > res) res = candidate;
         }
-        int result = binarySearch(nums,suff);
-        return result;
+        return res;
     }
 };
