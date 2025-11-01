8623132d447ca1b51ddfbabfb39ff83d802b1520
Ravi Kumar <ravi14577@gmail.com>
2025-11-01 16:57:39 +0530

    Time: 7 ms (97.37%), Space: 228.9 MB (97.98%) - LeetHub

--- 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.cpp
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.cpp
@@ -1,37 +1,16 @@
 class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
-        // Create an unordered_set for efficient lookup of values in nums
-        unordered_set<int> valuesToRemove(nums.begin(), nums.end());
-
-        // Handle the case where the head node needs to be removed
-        while (head != nullptr && valuesToRemove.count(head->val) > 0) {
-            ListNode* temp = head;
-            head = head->next;
-            delete temp;
+        bitset<100001> hasN=0;
+        for(int x: nums) hasN[x]=1;
+        ListNode dummy(0, head);
+        ListNode* prev=&dummy;
+        for(ListNode* curr=head; curr; curr=curr->next){
+            if (hasN[curr->val]) 
+                prev->next=curr->next;
+            else 
+                prev=prev->next;
         }
-
-        // If the list is empty after removing head nodes, return nullptr
-        if (head == nullptr) {
-            return nullptr;
-        }
-
-        // Iterate through the list, removing nodes with values in the set
-        ListNode* current = head;
-        while (current->next != nullptr) {
-            if (valuesToRemove.contains(current->next->val)) {
-                // Store the node to be deleted
-                ListNode* temp = current->next;
-                // Skip the next node by updating the pointer
-                current->next = current->next->next;
-                // Delete the removed node
-                delete temp;
-            } else {
-                // Move to the next node
-                current = current->next;
-            }
-        }
-
-        return head;
+        return dummy.next;
     }
 };
