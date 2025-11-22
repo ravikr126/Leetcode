5255bfba3c785361c07e1217902790be9ed1a547
Ravi Kumar <ravi14577@gmail.com>
2025-11-22 22:29:46 +0530

    Time: 0 ms (100%), Space: 9.3 MB (71.31%) - LeetHub

--- 3747-count-distinct-integers-after-removing-zeros/3747-count-distinct-integers-after-removing-zeros.cpp
+++ 3747-count-distinct-integers-after-removing-zeros/3747-count-distinct-integers-after-removing-zeros.cpp
@@ -1,32 +1,22 @@
-using ll = long long;
-
 class Solution {
 public:
     long long countDistinct(long long n) {
-    /*
-        see key idea is ans += (digit[i] - 1) * 9^(no. of digit to the right of digit[i])
-    */
-        if(n < 10) return n;
-
-        string s= to_string(n);
-
-        int L = s.size();
-
-        vector<ll> power_of_9(L+1,1);
-
-        for(int i=1;i<=L;i++) power_of_9[i] = power_of_9[i-1]*9;
-
-        ll ans=0;
-        // taking all numbers with one less digit of L i.e 1 digit , 2 digit ... (L-1) digit
-
-        for(int i=1;i<L;i++) ans += power_of_9[i];
-
-        for(int i=0;i<L;i++){ // go digit by digit and check how many posiible with current prefix.
-            ll d= s[i] - '0';
-
-            if(d > 1) ans += (d-1)*power_of_9[L-1-i]; // L-1-i is lenght of right of i
-            if(d==0) return ans;
+        long long ans =0;
+        string num = to_string(n);
+        if(n<10) return n;
+        int m = num.size();
+        for(int i=1;i<=m-1;i++){
+            ans=ans+(long long)pow(9,i);
         }
-        return ans+1; // cs above we are finding less then n, if n doesn't have '0' it is also valid cnt
+        // cout<<ans<<endl;
+        int index=0;
+        while(index<m && num[index]!='0'){
+            long long digit=num[index]-'0';
+            ans=ans+((digit-1)*(long long)pow(9,m-1-index));            
+            if(index==m-1) ans++;
+            index++;
+        }
+        return ans;
+        
     }
 };
