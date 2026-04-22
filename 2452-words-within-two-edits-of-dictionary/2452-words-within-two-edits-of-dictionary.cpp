4fe484b62d078f437fb8146cb9ecff5233aa5a13
Ravi Kumar <ravi14577@gmail.com>
2026-04-22 20:17:14 +0530

    Time: 19 ms (29.41%), Space: 25 MB (24.6%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
-        int n=queries[0].size();
-        vector<string> ans;
-        for(auto &it:queries){
-            for(auto &i:dictionary){
-                int diff=0;
-                for(int idx=0; idx<n; idx++){
-                    if(it[idx]!=i[idx]){
-                        diff++;
-                    }
-                    if(diff>2) break;
-                }
-                if(diff<=2){
-                    ans.push_back(it);
-                    break;
+    bool check(string q, string d){
+        int count = 0;
+        for(int i = 0; i < q.size(); i++){
+            if(q[i] != d[i]) count++;
+            if(count > 2) return false; // early exit
+        }
+        return true;
+    }
+
+    vector<string> twoEditWords(vector<string>& q, vector<string>& d) {
+        vector<string> s;
+
+        for(int i = 0; i < q.size(); i++){
+            for(int j = 0; j < d.size(); j++){
+                if(check(q[i], d[j])){
+                    s.push_back(q[i]);
+                    break; // 🔥 prevents duplicates
                 }
             }
         }
-        return ans;
+        return s;
     }
 };
