5038f866eb159b287469c44e47feeabcb2dfe190
Ravi Kumar <ravi14577@gmail.com>
2026-07-22 22:58:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int start=0;
-        int end=start+1;
-        vector<int> v;
-        while(start<nums.size()){
-            if(nums[start]+nums[end]==target){
-                // cout<<"hi"<<nums[start]<<" "<<nums[end]<<endl;
-                v.push_back(start);
-                v.push_back(end);
-                break;
-            }
-            if(end+1 < nums.size()){
-                end++;
-            }else{
-                start++;
-                end=start+1;
+        unordered_map<int, int> numMap;
+        int n = nums.size();
+
+     
+       
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.count(complement) && numMap[complement] != i) {
+                return {i, numMap[complement]};
             }
         }
-        return v;
+
+        return {}; 
     }
 };
